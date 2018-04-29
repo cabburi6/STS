1f1b2f0ff5dc009ba97ef97556b56d94444548cb
cabburi6 <cabburi6@gmail.com>
2018-04-29 04:05:54 -0400

    added custom server port

--- src/main/java/com/example/springboot/StudentApiApplication.java
+++ src/main/java/com/example/springboot/StudentApiApplication.java
@@ -3,6 +3,7 @@ package com.example.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//bootstrapping the spring application
 @SpringBootApplication
 public class StudentApiApplication {
 
